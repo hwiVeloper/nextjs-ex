3837db74655c36dbfe6ce43028741da9e12fc7a6
Jonghwi Lee <zziller03@gmail.com>
2019-08-07 00:39:21 +0900

    쿼리스트링을 활용한 dynamic한 페이지

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import withLayout from '../components/Layout';
+import Link from 'next/link';
 
-const Page = () => <p>Hello Next.js</p>;
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
-export default withLayout(Page);
+const Blog = () => {
+  return (
+    <div>
+      <h1>hwiveloper's Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="How to study react.js" />
+        <PostLink title="Javascript Framework Comparison" />
+      </ul>
+    </div>
+  )
+}
+
+export default withLayout(Blog);
---
+++ pages/post.js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router';
+import withLayout from '../components/Layout';
+
+const Content = () => {
+  const router = useRouter();
+  
+  return (
+    <>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </>
+  )
+}
+
+const Page = () => (
+  <Content/>
+);
+
+export default withLayout(Page);
